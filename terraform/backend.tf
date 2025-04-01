f1ec1b60dafac8e658766fc8f58f15d29cb88962
noluwo123 <noluwo123@gmail.com>
2025-04-01 15:19:45 +0100

    feat: tf feat

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "devopschallengetfstate"   # Replace with your S3 bucket name
     key     = "terraform/state/terraform.tfstate"  # Path within the bucket
-    region  = var.aws_region                 # Must match the region of your S3 bucket
+    region  = "us-east-1"                 # Must match the region of your S3 bucket
     encrypt = true
   }
 }
