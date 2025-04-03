160397d32f6bd5da1f39d2ef0ddc1de6befd66b5
noluwo123 <noluwo123@gmail.com>
2025-04-03 02:39:06 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://lightfeather-lb-1406138679.us-east-1.elb.amazonaws.com:8080/'
+export const API_URL = 'http://lightfeather-lb-1928965709.us-east-1.elb.amazonaws.com:8080/'
 export default API_URL
